914d083ba3cfefcbf1136e66444ebf7db31aa230
James Hensman <59737614+JamesHensman1@users.noreply.github.com>
2022-05-21 12:58:54 +0100

    Update headset.sh

--- headset.sh
+++ headset.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-
 sink=$(pactl info | grep "Default Sink" | awk '{print $NF}')
 IFS=. read name addr mode << EOF
 ${sink##*-}
 EOF
 device="$name.$addr"
-
 if [ $mode == "a2dp_sink" ]; then
 	espeak -z -vf4 -g 15 "Headset Mode"
 	pactl set-card-profile "bluez_card.$addr" headset_head_unit
 	pacmd set-default-sink "$device.headset_head_unit"
-
 else
 	espeak -z -vf4 -g 15 "Audio Mode"
 	pactl set-card-profile "bluez_card.$addr" a2dp_sink
 	pacmd set-default-sink "$device.a2dp_sink"
 fi 
-
-echo $device
-echo $mode
